4f4b1565bea90a4249e1f74f52a9fe5dc3334763
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-08 08:56:01 +0200

    added counter

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
 const inputContent = document.querySelector('.main__input');
 const mainButton = document.querySelector('.main__number');
 const feedback = document.querySelector('.main__feedback--text');
-
-//generar un número random e imprimirlo en la consola.
+const counter = document.querySelector('.main__counter');
 
 function randomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -12,11 +11,13 @@ function randomNumber(max) {
 
 const randomNumberactual = randomNumber(100);
 console.log(randomNumberactual);
-//al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
+
+let acc = 0; 
 
 function getInput() {
     let number = inputContent.value;
     console.log(number);
+    counter.innerHTML = acc++;
 
     if (number === '') {
         feedback.innerHTML = 'Por favor introduce un número';
@@ -30,10 +31,5 @@ function getInput() {
 }
 
 mainButton.addEventListener('click', getInput);
-
-
-//comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS GANADO, CAMPEONA!")
-
-
 
 //actualizar el contador de intentos cada vez que el usuario pruebe
