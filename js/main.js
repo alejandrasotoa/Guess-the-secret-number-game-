c5a4d003ea020fb29797ce3fb60a15a04eb17c17
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-08 08:38:31 +0200

    added first if of js

--- js/main.js
+++ js/main.js
@@ -2,23 +2,32 @@
 
 const inputContent = document.querySelector('.main__input');
 const mainButton = document.querySelector('.main__number');
+const feedback = document.querySelector('.main__feedback--text');
 
 //generar un número random e imprimirlo en la consola.
 
-function getRandomNumber(max) {
+const randomNumber =  (max) => {
     return Math.ceil(Math.random() * max);
 }
 
-console.log('> ' + getRandomNumber(100));
+console.log(randomNumber(100));
 
 //al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
 
 function getInput() {
-    console.log(inputContent.value);
+    let number = inputContent.value;
+    console.log(number);
+
+    if (number = randomNumber) {
+        feedback.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    }
 }
 
 mainButton.addEventListener('click', getInput);
 
 
 //comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS GANADO, CAMPEONA!")
+
+
+
 //actualizar el contador de intentos cada vez que el usuario pruebe
