056d161e961eb2b1fc617fe4921d4cf084b01219
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-08 08:23:47 +0200

    start js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+//generar un número random e imprimirlo en la consola.
+//al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
+//comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS GANADO, CAMPEONA!")
+//actualizar el contador de intentos cada vez que el usuario pruebe
