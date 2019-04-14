481c9cc51c2fa6325ed88b5960cf1b2087693a25
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-14 20:42:35 +0200

    disabled input and button

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const body = document.querySelector('body');
 const inputContent = document.querySelector('.main__input');
 const mainButton = document.querySelector('.main__button');
+// const resetButton = document.querySelector('.reset__button')
 const feedback = document.querySelector('.main__feedback--text');
 const counterItem = document.querySelector('.main__counter');
 const fireItem = document.querySelector('.fire__item');
@@ -24,6 +25,7 @@ const counter = () => {
 
 const startGame = () => {
     let number = parseInt(inputContent.value);
+    acc = 0;
     feedbackInfo(number);
 }
 
@@ -50,8 +52,15 @@ function fireClass() {
     iceItem.classList.add('hidden');
 }
 
-function feedbackInner(guess, text) {
-    feedback.innerHTML = `El número ${guess} está ${text}, intenta otra vez`;
+function win() {
+    body.classList.remove('ice', 'fire');
+    defaultItem.classList.remove('hidden');
+    iceItem.classList.add('hidden');
+    fireItem.classList.add('hidden');
+}
+
+function feedbackInner(guess, text, close) {
+    feedback.innerHTML = `El número ${guess} está ${text}, intenta uno más ${close}`;
 }
 
 function feedbackInfo(number) {
@@ -59,11 +68,22 @@ function feedbackInfo(number) {
         feedback.innerHTML = 'Por favor introduce un número';
     } else if (randomNumberactual === number) {
         feedback.innerHTML = '¡HAS GANADO!';
-    } else if (number <= (randomNumberactual + 10) && number >= (randomNumberactual - 10)) {
-        feedbackInner(number, 'cerca');
+        mainButton.disabled = true;
+        inputContent.disabled = true;
+        win()
+    } else if (number <= (randomNumberactual + 10) && number > randomNumberactual) {
+        feedbackInner(number, 'cerca', 'bajo');
+        fireClass();
+        counter();
+    } else if (number >= (randomNumberactual - 10) && number < randomNumberactual) {
+        feedbackInner(number, 'cerca', 'alto');
         fireClass();
         counter();
     } else if (number >= (randomNumberactual + 10)) {
+        feedbackInner(number, 'muy lejos', 'bajo');
+        iceClass();
+        counter();
+    } else {
         feedbackInner(number, 'muy lejos', 'alto');
         iceClass();
         counter();
