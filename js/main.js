e4db525791a4915dbe5701e75932856552f1280e
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-14 19:13:50 +0200

    change js, added animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const body = document.querySelector('body');
 const inputContent = document.querySelector('.main__input');
 const mainButton = document.querySelector('.main__number');
 const feedback = document.querySelector('.main__feedback--text');
-const counter = document.querySelector('.main__counter');
+const counterItem = document.querySelector('.main__counter');
+const fireItem = document.querySelector('.fire__item');
+const iceItem = document.querySelector('.ice__item');
+const defaultItem = document.querySelector('.default__item');
 
 function randomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -12,26 +16,48 @@ function randomNumber(max) {
 const randomNumberactual = randomNumber(100);
 console.log(randomNumberactual);
 
-let acc = 0; 
+let acc = 0;
+const counter = () =>{
+    acc++;
+    counterItem.innerHTML = acc;
+} 
 
 function getInput() {
     let number = parseInt(inputContent.value);
     console.log(number);
+    feedbackInfo(number);
+}
 
+function iceClass() {
+    defaultItem.classList.add('hidden');
+    body.classList.remove('fire');
+    body.classList.add('ice');
+    fireItem.classList.add('hidden');
+    iceItem.classList.remove('hidden');
+}
+
+function fireClass() {
+    defaultItem.classList.add('hidden');
+    body.classList.remove('ice');
+    body.classList.add('fire');
+    fireItem.classList.remove('hidden');
+    iceItem.classList.add('hidden');
+}
+
+function feedbackInfo(number) {
     if (Number.isNaN(number)) {
         feedback.innerHTML = 'Por favor introduce un número';
     } else if  (randomNumberactual === number) {
         feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
-    } else if (number > randomNumberactual) {
-        feedback.innerHTML = 'Demasiado alto, intenta otra vez';
-        acc++
-        counter.innerHTML = acc;
+    } else if (number <= (randomNumberactual + 10) && number >= (randomNumberactual - 10)) {
+        feedback.innerHTML = 'Estás cerca, intenta otra vez';
+        fireClass();
+        counter();
     } else {
-        feedback.innerHTML = 'Demasiado bajo, intenta otra vez';
-        acc++
-        counter.innerHTML = acc;
+        feedback.innerHTML = 'Muy lejos, intenta otra vez';
+        iceClass();
+        counter();
     }
 }
 
 mainButton.addEventListener('click', getInput);
-window.addEventListener('keypress', getInput);
